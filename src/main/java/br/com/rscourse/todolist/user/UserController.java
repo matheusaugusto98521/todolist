4de803363ea54c4e471b92e52034c8b98f26ba6a
ROCKETSEAT_JAVA <matheusferreirasoares1234@gmail.com>
2023-10-13 21:52:34 -0300

    #AULA-03

--- src/main/java/br/com/rscourse/todolist/user/UserController.java
+++ src/main/java/br/com/rscourse/todolist/user/UserController.java
@@ -2,12 +2,13 @@ package br.com.rscourse.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 /**
  * Modificador
@@ -31,6 +32,10 @@ public class UserController {
             System.out.println("User exists!");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User exists!");
         }
+
+        var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashed);
 
 
         var userCreated = this.userRepository.save(userModel);
